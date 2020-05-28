9bea832b798296c36503f577f129194203cb2d01
marksre <36451098+marksre@users.noreply.github.com>
2020-05-28 12:52:07 -0400

    added comments for BreakFiles.m

--- Step1_BreakFiles.m
+++ Step1_BreakFiles.m
@@ -1,9 +1,7 @@
+%% This script breaks one fNIRS data file into multiple files by block
 datadir=uigetdir();
 data=nirs.io.loadDirectory(datadir,{'Subject','Session'});
 
 for i=1:length(data)
     Blockbreaker(data(i).description);
 end
-
-
-
