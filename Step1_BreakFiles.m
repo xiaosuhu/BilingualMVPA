62869228e0e04a4e93ec51a3aae90f32b36bddac
marksre <36451098+marksre@users.noreply.github.com>
2020-05-28 13:34:28 -0400

    Added annotations to BreakFiles and GLMAnalysis

--- Step1_BreakFiles.m
+++ Step1_BreakFiles.m
@@ -1,4 +1,8 @@
 %% This script breaks one fNIRS data file into multiple files by block
+%% IMPORTANT NOTE:
+%  Original .nirs data file will be REPLACED by N smaller files (1 per
+%  block). Only run this script on a COPY of your original .nirs data.
+
 datadir=uigetdir();
 data=nirs.io.loadDirectory(datadir,{'Subject','Session'});
 
